e441a1edd45a377b72f87370099eda576cf076e1
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2025-03-04 17:31:53 +0530

    all file and folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'siddhanath tiwari',
+    author_email= 'siddhanathtiwari7709@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
